d0bb455749dd38e8f324cf87bbb283dd934867b4
iamcool12345 <96992097+iamcool12345@users.noreply.github.com>
2022-09-27 17:45:38 +0200

    Update BIOS.lua

--- Installer/BIOS.lua
+++ Installer/BIOS.lua
@@ -2,7 +2,7 @@
 local result, reason = ""
 
 do
-	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://raw.githubusercontent.com/iamcool12345/modos/master/Installer/Main.lua")
+	local handle, chunk = component.proxy(component.list("internet")() or error("INSTALL AN INTERNET CARD, YOU DAM NOOB!")).request("https://raw.githubusercontent.com/iamcool12345/modos/master/Installer/Main.lua")
 
 	while true do
 		chunk = handle.read(math.huge)
